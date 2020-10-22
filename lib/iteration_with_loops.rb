2cfa2dbfb4a308d032a3a140f4ef8526cebb3f68
Jason  Zhou <jzhou35@buffalo.edu>
2020-10-22 21:18:48 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ def find_even_values(src)
   while count < src.length do
     inner_count = 0
     while inner_count < src[count].length do
-      p if src[count][inner_count].even
+      p if src[count][inner_count].even?
     end
   end
 end
